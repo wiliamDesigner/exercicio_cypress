ad895707495f3a9b68d3a2c703f5d15a81aee764
WiliamDev <wiliam.omura3@hotmail.com>
2025-05-16 22:16:56 -0300

    1.0-pronto

---
+++ cypress/e2e/ebac.tarefa/tarefa.cy.js
@@ -0,0 +1,14 @@
+describe("Teste  de adição ,Remoção e Edição da pagina",()=>{
+    it('Deve Renderizar 3 campos de input',()=>{
+        cy.visit('https://agenda-contatos-react.vercel.app/')
+        cy.get('form input').eq(0).type('wiliam');      
+        cy.get('form input').eq(1).type('wiliam.omura3@hotmail.com');
+        cy.get('form input').eq(2).type('14997687459');
+        cy.get('form button.adicionar').click();
+        cy.get('div button.edit').click();
+        cy.get('form input').eq(0).type('-novo2');  
+        cy.get('div button.alterar').click();  
+        cy.get('div button.delete').click();  
+
+    })
+})
