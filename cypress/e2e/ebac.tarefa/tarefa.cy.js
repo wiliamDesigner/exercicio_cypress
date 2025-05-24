214508dc651c65b777808675d0ec5dbd7abcd79b
WiliamDev <wiliam.omura3@hotmail.com>
2025-05-24 14:39:17 -0300

    Update tarefa.cy.js

--- cypress/e2e/ebac.tarefa/tarefa.cy.js
+++ cypress/e2e/ebac.tarefa/tarefa.cy.js
@@ -1,14 +1,21 @@
-describe("Teste  de adição ,Remoção e Edição da pagina",()=>{
-    it('Deve Renderizar 3 campos de input',()=>{
-        cy.visit('https://agenda-contatos-react.vercel.app/')
-        cy.get('form input').eq(0).type('wiliam').should('have.prop', 'tagName').should('eq', 'INPUT')      
+describe("Teste de adição, Remoção e Edição da página", () => {
+    
+    it('Deve preencher as informações de um novo contato', () => {
+        cy.visit('https://agenda-contatos-react.vercel.app/');
+        cy.get('form input').eq(0).type('wiliam').should('have.prop', 'tagName').should('eq', 'INPUT');
         cy.get('form input').eq(1).type('wiliam.omura3@hotmail.com');
         cy.get('form input').eq(2).type('14997687459');
         cy.get('form button.adicionar').click();
-        cy.get('div button.edit').click();
-        cy.get('form input').eq(0).type('-novo2');  
-        cy.get('div button.alterar').click();  
-        cy.get('div button.delete').click();  
+    });
 
-    })
-})
+    it('Deve editar as informações de um contato', () => {
+        cy.get('.sc.-gueYoa.jWEbWB .button.edit').first().click()
+        cy.get('form input').eq(0).type('-novo2');
+        cy.get('div button.alterar').click();
+    });
+
+    it('Deve remover um contato', () => {
+        cy.get('div button.delete').first().click();
+    });
+
+});
